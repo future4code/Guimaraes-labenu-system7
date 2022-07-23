4f20ac3d2f3287c35a1b0bb1846adb0d75706a28
_crhis_ <crhisnokia@live.com>
2022-07-23 18:53:53 -0300

    feat: enpoint getClass funcionando, falta tipagem

--- src/data/Databases/ClassDataBase.ts
+++ src/data/Databases/ClassDataBase.ts
@@ -3,7 +3,6 @@ import { CustomError } from "../error/customError";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class ClassDataBase extends BaseDatabase {
-
   public createClass = async (newObject: {}): Promise<void> => {
     try {
       await BaseDatabase.connection("CLASS").insert(newObject);
@@ -14,14 +13,42 @@ export class ClassDataBase extends BaseDatabase {
 
   public getAllClass = async (): Promise<CLASS[]> => {
     try {
-      const result: CLASS[] = await BaseDatabase.connection("CLASS").orderBy(
-        "name"
-      );
-      return result;
+      let students;
+      let newArrayData: any = [];
+      const result: any = await BaseDatabase.connection
+        .select("c.id as classId", "c.name as class_Name", "c.module")
+        .from("CLASS as c")
+        .orderBy("name");
+      for (let element of result) {
+        students = await BaseDatabase.connection("STUDENTS as s")
+          .select(
+            "s.id as studentId",
+            "s.name",
+            "s.email",
+            "s.birth_date as birthDate",
+            "s.class_id"
+          )
+          .where("s.class_id", element.classId);
+
+        for (const student of students) {
+          let newDateSplit = new Date(student.birthDate)
+            .toISOString()
+            .split("T");
+          let newBirthDate = newDateSplit[0].split("-").reverse().join("/");
+          student.birthDate = newBirthDate;
+        }
+
+        newArrayData.push({
+          id: element.classId,
+          name: element.class_Name,
+          module: element.module,
+          teachers: element.teachers,
+          students,
+        });
+      }
+      return newArrayData;
     } catch (error: any) {
       throw new CustomError(error.sqlMessage || error.message, 500);
     }
   };
-
-
 }
